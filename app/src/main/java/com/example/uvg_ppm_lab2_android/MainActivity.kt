f7e45f0ea654558371db94e658fdbcfd064215a9
Adrian-24914 <pen24914@uvg.edu.gt>
2025-07-31 21:01:18 -0600

    Inicio del proyecto, estructura base

--- app/src/main/java/com/example/uvg_ppm_lab2_android/MainActivity.kt
+++ app/src/main/java/com/example/uvg_ppm_lab2_android/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.uvg_ppm_lab2_android
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.uvg_ppm_lab2_android.ui.theme.Uvgppmlab2androidTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Uvgppmlab2androidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Home(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +31,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Home(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Proyecto UVG - Lab 2",
+            fontSize = 26.sp,
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "¡Hello World!",
+            fontSize = 18.sp
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(onClick = {
+            Toast.makeText(context, "¡Gracias por presionar el botón!", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Haz clic aquí")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomePreview() {
     Uvgppmlab2androidTheme {
-        Greeting("Android")
+        Home()
     }
 }
